3cb9d11c0e7621581cae4947aa0e4131cb208f42
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-06 15:21:03 +0700

    Update managers.py

--- userbot/core/managers.py
+++ userbot/core/managers.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 
 from ..helpers.utils.format import md_to_text, paste_message
-from .data import _sudousers_list
+
 
 
 # https://t.me/c/1220993104/623253
@@ -19,12 +19,10 @@ async def edit_or_reply(
     linktext=None,
     caption=None,
 ):  # sourcery no-metrics
-    sudo_users = _sudousers_list()
     link_preview = link_preview or False
     reply_to = await event.get_reply_message()
     if len(text) < 4096 and not deflink:
         parse_mode = parse_mode or "md"
-        if event.sender_id in sudo_users:
             if reply_to:
                 return await reply_to.reply(
                     text, parse_mode=parse_mode, link_preview=link_preview
@@ -40,7 +38,6 @@ async def edit_or_reply(
         linktext = linktext or "Message was to big so pasted to bin"
         response = await paste_message(text, pastetype="s")
         text = linktext + f" [here]({response})"
-        if event.sender_id in sudo_users:
             if reply_to:
                 return await reply_to.reply(text, link_preview=link_preview)
             return await event.reply(text, link_preview=link_preview)
@@ -54,24 +51,15 @@ async def edit_or_reply(
         await reply_to.reply(caption, file=file_name)
         await event.delete()
         return os.remove(file_name)
-    if event.sender_id in sudo_users:
-        await event.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
     await event.client.send_file(event.chat_id, file_name, caption=caption)
     await event.delete()
     os.remove(file_name)
 
 
 async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
-    sudo_users = _sudousers_list()
     parse_mode = parse_mode or "md"
     link_preview = link_preview or False
     time = time or 5
-    if event.sender_id in sudo_users:
-        reply_to = await event.get_reply_message()
-        catevent = (
-            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
             if reply_to
             else await event.reply(
                 text, link_preview=link_preview, parse_mode=parse_mode
